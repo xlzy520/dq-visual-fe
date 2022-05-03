2453f1ddb68767bb1be3ed5d18c4e7d72fcb754f
xlz <20794489@qq.com>
2022-05-03 19:13:01 +0800

    订单列表新增已取消，客户列表建立中

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* Layout */
-import Layout from '@/layout'
+import Layout from '@/layout';
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -34,25 +34,27 @@ export const constantRoutes = [
   {
     path: '/login',
     component: () => import('@/views/login/index'),
-    hidden: true
+    hidden: true,
   },
 
   {
     path: '/404',
     component: () => import('@/views/404'),
-    hidden: true
+    hidden: true,
   },
 
   {
     path: '/',
     component: Layout,
     redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
-    }]
+    children: [
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import('@/views/dashboard/index'),
+        meta: { title: 'Dashboard', icon: 'dashboard' },
+      },
+    ],
   },
 
   {
@@ -63,18 +65,24 @@ export const constantRoutes = [
     meta: { title: 'Example', icon: 'el-icon-s-help' },
     children: [
       {
-        path: 'table',
-        name: 'Table',
+        path: '订单列表',
+        name: '订单列表',
         component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
+        meta: { title: '订单列表', icon: 'table' },
+      },
+      {
+        path: '用户列表',
+        name: '用户列表',
+        component: () => import('@/views/table/index2'),
+        meta: { title: '用户列表', icon: 'table' },
       },
       {
         path: 'tree',
         name: 'Tree',
         component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
+        meta: { title: 'Tree', icon: 'tree' },
+      },
+    ],
   },
 
   {
@@ -85,9 +93,9 @@ export const constantRoutes = [
         path: 'index',
         name: 'Form',
         component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
+        meta: { title: 'Form', icon: 'form' },
+      },
+    ],
   },
 
   {
@@ -97,7 +105,7 @@ export const constantRoutes = [
     name: 'Nested',
     meta: {
       title: 'Nested',
-      icon: 'nested'
+      icon: 'nested',
     },
     children: [
       {
@@ -110,7 +118,7 @@ export const constantRoutes = [
             path: 'menu1-1',
             component: () => import('@/views/nested/menu1/menu1-1'),
             name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
+            meta: { title: 'Menu1-1' },
           },
           {
             path: 'menu1-2',
@@ -122,31 +130,31 @@ export const constantRoutes = [
                 path: 'menu1-2-1',
                 component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
                 name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
+                meta: { title: 'Menu1-2-1' },
               },
               {
                 path: 'menu1-2-2',
                 component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
                 name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
+                meta: { title: 'Menu1-2-2' },
+              },
+            ],
           },
           {
             path: 'menu1-3',
             component: () => import('@/views/nested/menu1/menu1-3'),
             name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
+            meta: { title: 'Menu1-3' },
+          },
+        ],
       },
       {
         path: 'menu2',
         component: () => import('@/views/nested/menu2/index'),
         name: 'Menu2',
-        meta: { title: 'menu2' }
-      }
-    ]
+        meta: { title: 'menu2' },
+      },
+    ],
   },
 
   {
@@ -155,27 +163,28 @@ export const constantRoutes = [
     children: [
       {
         path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
-      }
-    ]
+        meta: { title: 'External Link', icon: 'link' },
+      },
+    ],
   },
 
   // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
-]
+  { path: '*', redirect: '/404', hidden: true },
+];
 
-const createRouter = () => new Router({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
-})
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes,
+  });
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
 }
 
-export default router
+export default router;
--- src/views/table/index2.vue
+++ src/views/table/index2.vue
@@ -3,19 +3,11 @@
     <!--    搜索栏-->
     <div class="header">
       <el-form :inline="true" :model="formInline" class="demo-form-inline" ref="form">
-        <el-form-item label="ID" prop="id">
-          <el-input v-model="formInline.id" placeholder="ID" />
+        <el-form-item label="店铺名称" prop="shopName">
+          <el-input v-model="formInline.shopName" placeholder="店铺名称" />
         </el-form-item>
-        <el-form-item label="姓名" prop="name">
+        <el-form-item label="用户名称" prop="name">
           <el-input v-model="formInline.name"></el-input>
-        </el-form-item>
-        <el-form-item label="支付状态" prop="status">
-          <el-select v-model="formInline.status" placeholder="支付状态">
-            <el-option label="待支付" value="1" />
-            <el-option label="支付成功" value="2" />
-            <el-option label="已发货" value="3" />
-            <el-option label="已取消" value="4" />
-          </el-select>
         </el-form-item>
         <el-form-item>
           <!--          搜索：先把要搜索的数据传给后台，后台反馈回来，把后台数据赋值给表格数据-->
@@ -33,12 +25,12 @@
     </div>
     <!--    列表栏-->
     <el-table v-loading="loading" :data="tableData" element-loading-text="Loading" border fit highlight-current-row>
-      <el-table-column prop="id" align="center" label="ID" width="95"> </el-table-column>
-      <el-table-column prop="shopName" label="店铺名称" width="120" />
-      <el-table-column prop="name" label="用户名称" width="100" />
+      <el-table-column prop="numbering" align="center" label="ID" width="95"> </el-table-column>
+      <el-table-column prop="name1" label="店铺名称" width="120" />
+      <el-table-column prop="name2" label="用户名称" width="100" />
       <el-table-column prop="price" label="订单价格" width="100" />
       <el-table-column prop="PaymentStatus" label="支付状态" width="110" />
-      <el-table-column prop="shopAddress" label="店铺地址" width="250" />
+      <el-table-column prop="address2" label="店铺地址" width="250" />
       <el-table-column prop="address1" label="收货地址" width="250" />
       <el-table-column prop="action" label="操作" align="center">
         <!--slot插槽，用#代替了并给了参数row，这里都用row来写        -->
@@ -70,34 +62,26 @@
     </div>
     <!--    新增栏/编辑栏 是2.几版本的和3,。几的不一样-->
     <!--    :visible.sync和v-model-->
-    <el-dialog title="信息编辑" :visible.sync="dialogFormVisible">
+    <el-dialog title="客户信息编辑" :visible.sync="dialogFormVisible">
       <el-form ref="dialogForm" :model="form" label-width="80px" class="form">
         <el-form-item label="店铺名称">
           <el-input v-model="form.shopName" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="店铺地址">
+          <el-input v-model="form.shopAddress" autocomplete="off"></el-input>
+        </el-form-item>
         <el-form-item label="用户名称">
           <el-input v-model="form.name" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item label="订单价格">
-          <el-input v-model="form.price" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="支付状态" prop="status">
-          <el-select v-model="form.PaymentStatus" placeholder="支付状态">
-            <el-option label="待支付" value="1" />
-            <el-option label="支付成功" value="2" />
-            <el-option label="已发货" value="3" />
-            <el-option label="已取消" value="4" />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="店铺地址">
-          <el-input v-model="form.shopAddress" autocomplete="off"></el-input>
+        <el-form-item label="联系电话">
+          <el-input v-model="form.phoneNumber" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="收货地址">
           <el-input v-model="form.address1" autocomplete="off"></el-input>
         </el-form-item>
-        <!--        <el-form-item label="备注">-->
-        <!--          <el-input v-model="form.remarks" autocomplete="off" type="textarea"></el-input>-->
-        <!--        </el-form-item>-->
+        <el-form-item label="备注">
+          <el-input v-model="form.remarks" autocomplete="off" type="textarea"></el-input>
+        </el-form-item>
       </el-form>
       <template #footer>
         <!--        当他为f的时候看不见，当他为T的时候出现-->
@@ -128,9 +112,8 @@ export default {
     return {
       //搜索栏
       formInline: {
-        id: '',
+        shopName: '',
         name: '',
-        status: '',
       },
       //新增栏
       type: 'add', // edit
@@ -140,8 +123,8 @@ export default {
         name: '', //用户名称
         shopAddress: '', //店铺地址
         address1: '', //收货地址
-        price: '', //price价格
-        PaymentStatus: '', //支付状态
+        phoneNumber: '', //电话号码
+        remarks: '', //备注
       },
       loading: false,
       tableData: null,
@@ -164,9 +147,8 @@ export default {
       //还未做
       axios
         .post('http://localhost:5000/users/page', {
-          id: this.formInline.id,
+          shopName: this.formInline.shopName,
           name: this.formInline.name,
-          PaymentStatus: this.formInline.PaymentStatus,
           pageNum: this.currentPage, //在第几页找
           pageSize: 10,
         })
@@ -177,108 +159,108 @@ export default {
           const mockData = [
             {
               id: 1,
-              shopName: 'aaa',
-              name: 'a',
-              shopAddress: '京',
+              name1: 'aaa',
+              name2: 'a',
+              address2: '京',
               address1: '北京',
               price: 18.0,
               PaymentStatus: '支付成功',
             },
             {
               id: 2,
-              shopName: 'bbb',
-              name: 'b',
-              shopAddress: '上海',
+              name1: 'bbb',
+              name2: 'b',
+              address2: '上海',
               address1: '上海',
               price: 15.0,
               PaymentStatus: '待支付',
             },
             {
               id: 3,
-              shopName: 'ccc',
-              name: 'c',
-              shopAddress: '长沙',
+              name1: 'ccc',
+              name2: 'c',
+              address2: '长沙',
               address1: '长沙',
               price: 10.0,
               PaymentStatus: '已发货',
             },
             {
               id: 4,
-              shopName: 'ddd',
-              name: 'd',
-              shopAddress: '北京',
+              name1: 'ddd',
+              name2: 'd',
+              address2: '北京',
               address1: '北京',
               price: 18.0,
               PaymentStatus: '支付成功',
             },
             {
               id: 5,
-              shopName: 'eee',
-              name: 'e',
-              shopAddress: '北京',
+              name1: 'eee',
+              name2: 'e',
+              address2: '北京',
               address1: '北京',
               price: 18.0,
               PaymentStatus: '支付成功',
             },
             {
               id: 6,
-              shopName: 'fff',
-              name: 'f',
-              shopAddress: '北京',
+              name1: 'fff',
+              name2: 'f',
+              address2: '北京',
               address1: '北京',
               price: 18.0,
               PaymentStatus: '支付成功',
             },
             {
               id: 7,
-              shopName: 'ggg',
-              name: 'g',
-              shopAddress: '北京',
+              name1: 'ggg',
+              name2: 'g',
+              address2: '北京',
               address1: '北京',
               price: 18.0,
               PaymentStatus: '支付成功',
             },
             {
               id: 8,
-              shopName: 'hhh',
-              name: 'h',
-              shopAddress: '北京',
+              name1: 'hhh',
+              name2: 'h',
+              address2: '北京',
               address1: '北京',
               price: 18.0,
               PaymentStatus: '支付成功',
             },
             {
               id: 9,
-              shopName: 'iii',
-              name: 'i',
-              shopAddress: '北京',
+              name1: 'iii',
+              name2: 'i',
+              address2: '北京',
               address1: '北京',
               price: 18.0,
               PaymentStatus: '支付成功',
             },
             {
               id: 10,
-              shopName: 'jjj',
-              name: 'j',
-              shopAddress: '北京',
+              name1: 'jjj',
+              name2: 'j',
+              address2: '北京',
               address1: '北京',
               price: 18.0,
               PaymentStatus: '支付成功',
             },
             {
               id: 11,
-              shopName: 'lll',
-              name: 'l',
-              shopAddress: '北京',
+              name1: 'lll',
+              name2: 'l',
+              address2: '北京',
               address1: '北京',
               price: 18.0,
               PaymentStatus: '支付成功',
             },
             {
               id: 12,
-              shopName: 'mmm',
-              name: 'm',
-              shopAddress: '北京',
+              name1: 'mmm',
+              name2: 'm',
+              address2: '北京',
               address1: '北京',
               price: 18.0,
               PaymentStatus: '支付成功',
@@ -310,14 +292,12 @@ export default {
         //，后台网站地址暂时不知道，网址虚的
         .post('http://localhost:5000/users/' + this.type, {
           //发送的
-
-          id: this.form.id,
           shopName: this.form.shopName,
           name: this.form.name,
           shopAddress: this.form.shopAddress,
           address1: this.form.address1,
-          price: this.form.price,
-          PaymentStatus: this.form.PaymentStatus,
+          phoneNumber: this.form.phoneNumber,
+          remarks: this.form.remarks,
         })
         //发送成功，然后做什么，没有成功，不会做下面的方法
         .then((res) => {
