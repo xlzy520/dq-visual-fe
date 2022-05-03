e89fd720bcf82de657ed652ff89165cafa5a23e7
xlz <20794489@qq.com>
2022-05-03 18:00:42 +0800

    table界面

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -47,15 +47,26 @@
             title="确定删除吗?"
             confirm-button-text="确定"
             cancel-button-text="取消"
-            @confirm="remove(row)"
+            @onConfirm="remove(row)"
           >
-            <template #reference>
-              <el-button size="mini" type="danger" class="remove">移除</el-button>
-            </template>
+            <el-button slot="reference" size="mini" type="danger" class="remove">移除</el-button>
           </el-popconfirm>
         </template>
       </el-table-column>
     </el-table>
+    <!--    分页-->
+    <!--    <el-pagination-->
+    <!--      v-model:currentPage="currentPage"-->
+    <!--      :page-size="10"-->
+    <!--      layout="total, prev, pager, next"-->
+    <!--      :total="total"-->
+    <!--      @current-change="getData"-->
+    <!--    />-->
+    <div class="block">
+      <!--      <span class="demonstration">页数较少时的效果</span>-->
+      <el-pagination :page-size="10" layout="prev, pager, next" :total="50" @current-change="changePage">
+      </el-pagination>
+    </div>
     <!--    新增栏/编辑栏 是2.几版本的和3,。几的不一样-->
     <!--    :visible.sync和v-model-->
     <el-dialog title="信息编辑" :visible.sync="dialogFormVisible">
@@ -127,12 +138,10 @@ export default {
         name2: '', //用户名称
         address2: '', //店铺地址
         address1: '', //收货地址
-        //price价格
-        price: '',
+        price: '', //price价格
         PaymentStatus: '', //支付状态
       },
       loading: false,
-      //不知道有没有用
       tableData: null,
       currentPage: 1,
     };
@@ -141,6 +150,12 @@ export default {
     this.getData();
   },
   methods: {
+    //分页，把value传过去
+    changePage(val) {
+      console.log(val, '===========打印的 ------ changePage');
+      this.currentPage = val;
+      this.getData();
+    },
     //搜索
     getData() {
       this.loading = true;
@@ -293,10 +308,14 @@ export default {
         //，后台网站地址暂时不知道，网址虚的
         .post('http://localhost:5000/users/' + this.type, {
           //发送的
+
           id: this.form.id,
-          name: this.form.name,
+          name1: this.form.name1,
+          name2: this.form.name2,
+          address2: this.form.address2,
+          address1: this.form.address1,
           price: this.form.price,
-          newStatus: this.form.newStatus,
+          PaymentStatus: this.form.PaymentStatus,
         })
         //发送成功，然后做什么，没有成功，不会做下面的方法
         .then((res) => {
@@ -311,21 +330,12 @@ export default {
           }
         });
     },
-    //编辑  未完成
+    //编辑
     edit(row) {
       this.dialogFormVisible = true;
       this.type = 'edit';
       console.log(row, '===========打印的 ------ edit');
-      // this.form=this.tableData
-      this.form.id = row.id;
-      this.form.name1 = row.name1;
-      this.form.name2 = row.name2;
-      this.form.address2 = row.address2;
-      this.form.address1 = row.address1;
-      // this.form.remarks = row.remarks;
-      this.form.price = row.price;
-      this.form.PaymentStatus = row.PaymentStatus;
-      // this.form = this.tableData;
+      this.form = row;
     },
     //删除
     remove(item) {
@@ -336,17 +346,8 @@ export default {
           id: item.id,
         })
         .then((res) => {
-          this.tableData.splice(index, 1);
           this.getData();
         });
-    },
-    //不知道有没有用
-    fetchData() {
-      this.listLoading = true;
-      getList().then((response) => {
-        this.list = response.data.items;
-        this.listLoading = false;
-      });
     },
   },
 };
