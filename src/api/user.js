70a09e617ca191609f14bdb6887e5c00d885ec05
xlz <20794489@qq.com>
2022-05-05 21:53:08 +0800

    feat: 登录联调

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,24 @@
-import request from '@/utils/request'
+import request from '@/utils/request';
 
 export function login(data) {
   return request({
-    url: '/vue-admin-template/user/login',
+    url: 'user/login',
     method: 'post',
-    data
-  })
+    data,
+  });
 }
 
 export function getInfo(token) {
   return request({
-    url: '/vue-admin-template/user/info',
+    url: 'user/info',
     method: 'get',
-    params: { token }
-  })
+    params: { token },
+  });
 }
 
 export function logout() {
   return request({
     url: '/vue-admin-template/user/logout',
-    method: 'post'
-  })
+    method: 'post',
+  });
 }
